162f1b797b247fcb37e7ad796b61a6940786da0e
only-wl <63798165+only-wl@users.noreply.github.com>
2022-03-19 18:30:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,13 +11,13 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i 's/OpenWrt/HC5962/g' package/base-files/files/bin/config_generate 
+#sed -i 's/OpenWrt/HC5962/g' package/base-files/files/bin/config_generate 
 
 # Modify ssidname
-sed -i 's/OpenWrt/HiWiFi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/OpenWrt/HiWiFi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 修改默认主题为material
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
