87a63e69681e9769233dff2d13ea595d336fd3d0
only-wl <63798165+only-wl@users.noreply.github.com>
2022-04-17 23:49:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 
+#sed -i 's/openwrt.xyz/hiwifi.com/g' /package/base-files/files/etc/hotplug.d/iface/11-gateway 
+
 # Modify hostname
 #sed -i 's/OpenWrt/HC5962/g' package/base-files/files/bin/config_generate 
 
@@ -20,4 +22,4 @@
 #sed -i 's/OpenWrt/HiWiFi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 修改默认主题为material
-#sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
