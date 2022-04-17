dc3aa6ce725b50e40e2fb17c07eed27847f56aa5
only-wl <63798165+only-wl@users.noreply.github.com>
2022-04-17 23:49:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
-
 #sed -i 's/openwrt.xyz/hiwifi.com/g' /package/base-files/files/etc/hotplug.d/iface/11-gateway 
 
 # Modify hostname
